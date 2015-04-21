5c8a9e35c62db2d3f8113c0ebd9625c8b0bb611a
William RUGGIERO <ruggierowilliam@gmail.com>
2015-04-21 16:55:14 +0200

    Page Accueil intégrée à play

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,13 +9,10 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        // return ok(index.render("Your new application is ready."));
-        return redirect(routes.Application.home());
+         return ok(index.render("Your new application is ready."));   
     }
     
-    public static Result home() {
-    	home();
-    }
+    
 
 }
 
