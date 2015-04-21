81a7a4de804aa25faaf025926e04dc9217fc3976
William RUGGIERO <ruggierowilliam@gmail.com>
2015-04-21 11:46:09 +0200

    Ajout du dossier bootstrap

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+
 import play.mvc.*;
 
 import views.html.*;
@@ -8,7 +9,13 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        // return ok(index.render("Your new application is ready."));
+        return redirect(routes.Application.home());
+    }
+    
+    public static Result home() {
+    	home();
     }
 
 }
+
