72a6edf535aabc1119893c7424158ef9ca5ac52c
William RUGGIERO <ruggierowilliam@gmail.com>
2015-05-06 09:18:53 +0200

    Ajout de nouveaux controleurs, controleur offres + nouvelles view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,8 +9,10 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-         return ok(index.render("Your new application is ready."));   
+         return ok(index.render());   
     }
+    
+ 
     
     
 
