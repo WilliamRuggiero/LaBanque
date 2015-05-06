6bc5be2222aed2999bd580d07a0a8c2f9502729e
William RUGGIERO <ruggierowilliam@gmail.com>
2015-05-06 09:23:42 +0200

    Ajout des nouvelles vues et pages css

---
+++ app/controllers/Offres.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import play.*;
+
+import play.mvc.*;
+
+import views.html.*;
+
+	public class Offres extends Controller {
+
+	public static Result CompteCourant() {
+	    	return ok(CompteCourant.render());
+	    }
+	public static Result LivretA() {
+	    	return ok(LivretA.render());
+	    }
+	public static Result Cartes() {
+	    	return ok(Cartes.render());
+	    }
+	public static Result PEL() {
+	    	return ok(PEL.render());
+	    }
+
+
+}
