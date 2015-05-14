3bd7edfca917ef6ef84270f347f8455bd5bc96dd
Jacques Arthur MOMBO <jacquesarthur.mombo@gmail.com>
2015-05-14 13:24:35 +0200

    Ajout de la page Rappel Mot de passe

--- app/controllers/Inscription.java
+++ app/controllers/Inscription.java
@@ -7,8 +7,11 @@ import views.html.*;
 	public class Inscription extends Controller {
 
 	public static Result Inscri() {
-	    	return ok(Inscri.render());
-	    }
+	    return ok(Inscri.render());
+	}
+	public static Result RappelMotDePasse() {
+    	return ok(MotDePasse.render());
+    }
 	
 	
 	
