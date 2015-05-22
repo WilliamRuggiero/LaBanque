7746a06faae20decaaf6fe4701bd32f210480816
Jacques Arthur MOMBO <jacquesarthur.mombo@gmail.com>
2015-05-22 03:18:36 +0200

    Page de connexion au BackOffice

---
+++ app/controllers/LoginBackEnd.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.User;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import formData.userLogin.UserLogin;
+import formData.userRegister.NewUserData;
+import views.html.page.backEnd.register.*;
+
+public class LoginBackEnd extends Controller {
+	
+	public static Result indexAdmin(){
+		
+		UserLogin formData = new UserLogin();
+
+		Form<UserLogin> form = Form.form(UserLogin.class).fill(formData);
+		return ok(loginAdmin.render(form));
+	}
+	
+	public static Result connectBackOffice() {
+    	Form<UserLogin> form = Form.form(UserLogin.class).bindFromRequest();
+    	
+    	if(form.hasErrors()) {
+    		return ok(loginAdmin.render(form));
+    	} 
+    	else {
+        	UserLogin data = form.get();
+        	//return redirect(routes.index());		
+        	return ok(loginAdmin.render(form));
+    	}
+    }
+}
