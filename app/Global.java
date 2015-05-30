3415ef85d6c74de0d115b46e19692b81d76afd33
William RUGGIERO <ruggierowilliam@gmail.com>
2015-05-30 13:02:32 +0200

    Refonte offre

--- app/Global.java
+++ app/Global.java
@@ -12,11 +12,11 @@ public class Global extends GlobalSettings {
     
     System.out.print("toto");
     
-    User u = new User((long) 1,"William","RUGGIERO","ruggierowilliam@gmail.com","0622030466","azerty","AA","male",true);
-    User v = new User((long) 1,"William","RUGGIERO","ruggierowilliam@gmail.com","0622030466","azerty","AA","male",false);
+   // User u = new User((long) 1,"William","RUGGIERO","ruggierowilliam@gmail.com","0622030466","azerty","AA","male",true);
+   // User v = new User((long) 2,"William","RUGGIERO","ruggierowilliam@gmail.fr","0622030466","azerty","AA","male",false);
 	
-    u.save();
-	v.save();
+   // u.save();
+   // v.save();
   
   
 }
